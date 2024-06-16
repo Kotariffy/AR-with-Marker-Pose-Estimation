28f6a69c0580f15d6b4a42d6f8dd3ab9e46a8664
Kotari <Kotariffy@yandex.ru>
2024-06-16 13:01:08 +0300

    Calculated euler angles (roll, pitch, yaw) from Rodrigues rotation matrix.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2 as cv
 from cv2 import aruco
+from math import (
+    asin, pi, atan2, sqrt, cos
+)
 
 ARUCO_DICT = {
     "DICT_4x4_50": cv.aruco.DICT_4X4_50,
@@ -17,11 +20,11 @@ ARUCO_DICT = {
     "DICT_6x6_1000": cv.aruco.DICT_6X6_1000
 }
     
-
 BLUE = (255, 50, 50)
 GREEN = (50, 255, 50)
 RED = (50, 50, 255)
 WHITE = (255, 255, 255)
+PURPLE = (255, 50, 255)
 
 def marker_display(corners, ids, rejected, image): 
     if len(corners) > 0:
@@ -42,10 +45,11 @@ def marker_display(corners, ids, rejected, image):
                 cv.line(image, bottomRight, bottomLeft, GREEN, 2)
                 cv.line(image, bottomLeft, topLeft, GREEN, 2)
 
+                # compute and draw the center (x, y) - coordinates of the ArUco marker
                 cX = int((topLeft[0] + bottomRight[0]) / 2.0)
                 cY = int((topLeft[1] + bottomRight[1]) / 2.0)
 
-                cv.circle(image, (cX, cY), 4, BLUE, 2)
+                cv.circle(image, (cX, cY), 4, PURPLE, -1)
 
                 cv.putText(image, str(markerID), (topLeft[0], topLeft[1] - 10),
                         cv.FONT_HERSHEY_PLAIN,
@@ -54,19 +58,21 @@ def marker_display(corners, ids, rejected, image):
                         2)
     return image
 
-def print_coordinates(id, x, y, z, angleX, angleY, angleZ):
+# roll -> rotation about x-axis (RED axis)
+# pitch -> rotation about y-axis (GREEN axis)
+# yaw -> rotation about z-axis (BLUE axis)
+def print_coordinates(id, x, y, z, roll, pitch, yaw):
     print("---")
     print("marker i = ", id)
     print("coordinates:")
     print("  - position:")
-    print('      - X: {:.3f}'.format(x))
-    print('      - Y: {:.3f}'.format(y))
-    print('      - H: {:.3f}'.format(z))
+    print('      - X:       {:.3f}'.format(x))
+    print('      - Y:       {:.3f}'.format(y))
+    print('      - H:       {:.3f}'.format(z))
     print("  - orientation:")
-    print('      - wX: {:.3f}'.format(angleX * 57.2958))
-    print('      - wY: {:.3f}'.format(angleY * 57.2958))
-    print('      - wZ: {:.3f}'.format(angleZ * 57.2958))
-
+    print('      - roll:    {:.3f}'.format(roll))
+    print('      - pitch:   {:.3f}'.format(pitch))
+    print('      - yaw:     {:.3f}'.format(yaw))
 
 def main():
 
@@ -100,7 +106,7 @@ def main():
             total_markers = range(0, ids.size)
             for ids, corners, i in zip(ids, corners, total_markers):
                 
-                # Drawing the pose of the marker
+                # Drawing axisies for the pose of the marker
                 _ = cv.drawFrameAxes(img, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)
 
                 corners = corners.reshape(4,2)
@@ -108,23 +114,88 @@ def main():
                 top_right = corners[0].ravel()
                 bottom_right = corners[2].ravel()
 
-                cv.putText(img, f"id: {ids[0]} Dist: {round(tVec[i][0][2], 2)}", top_right, cv.FONT_HERSHEY_PLAIN, 1.3, (200, 100, 0), 2)
-                cv.putText(img, f"x: {round(tVec[i][0][0], 1)} y: {round(tVec[i][0][1], 1)}", bottom_right, cv.FONT_HERSHEY_PLAIN, 1.3, (200, 100, 0), 2)
+                cv.putText(img, f"id: {ids[0]} Dist: {round(tVec[i][0][2], 2)}", top_right, cv.FONT_HERSHEY_PLAIN, 1.3, PURPLE, 2)
+                cv.putText(img, f"x: {round(tVec[i][0][0], 3)} y: {round(tVec[i][0][1], 3)}", bottom_right, cv.FONT_HERSHEY_PLAIN, 1.3, PURPLE, 2)
                 
                 x       = tVec[i][0][0]
                 y       = tVec[i][0][1]
                 z       = tVec[i][0][2]
-                angleX  = rVec[i][0][0] 
-                angleY  = rVec[i][0][1] 
-                angleZ  = rVec[i][0][2] 
 
-                print_coordinates(i, x, y, z, angleX, angleY, angleZ)
+                rotation_matrix, _ = cv.Rodrigues(rVec)
+                angle_vec = calculateEulerAngle(rotation_matrix)
+             
+                print_coordinates(i, x, y, z, angle_vec[0], angle_vec[1], angle_vec[2])
 
 
         # Display the resulting frame with axies.
         cv.imshow('Main window', img)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
+
+
+def calculateEulerAngle(rotation_matrix):
+
+    R11 = rotation_matrix[0][0]
+    R12 = rotation_matrix[0][1]
+    R13 = rotation_matrix[0][2]
+    R21 = rotation_matrix[1][0]
+    R22 = rotation_matrix[1][1]
+    R23 = rotation_matrix[1][2]
+    R31 = rotation_matrix[2][0]
+    R32 = rotation_matrix[2][1]
+    R33 = rotation_matrix[2][2]
+
+    if R31 != 1 and R31 != -1: 
+        pitch_1 = -1*asin(R31)
+        pitch_2 = pi - pitch_1 
+        roll_1 = atan2( R32 / cos(pitch_1) , R33 /cos(pitch_1)) 
+        roll_2 = atan2( R32 / cos(pitch_2) , R33 /cos(pitch_2)) 
+        yaw_1 = atan2( R21 / cos(pitch_1) , R11 / cos(pitch_1))
+        yaw_2 = atan2( R21 / cos(pitch_2) , R11 / cos(pitch_2)) 
+
+        pitch = pitch_1 
+        roll = roll_1
+        yaw = yaw_1 
+    else: 
+        yaw = 0 # anything (we default this to zero)
+        if R31 == -1: 
+            pitch = pi/2 
+            roll = yaw + atan2(R12,R13) 
+        else: 
+            pitch = -pi/2 
+            roll = -1 * yaw + atan2(-1 * R12,-1 * R13) 
+
+    # convert from radians to degrees
+    roll = roll * 180/pi 
+    pitch = pitch * 180/pi
+    yaw = yaw * 180/pi 
+    rxyz_deg = [roll , pitch , yaw]
+
+    return rxyz_deg
+
+def calculateEulerAngleSimply(rotation_matrix):
+    R11 = rotation_matrix[0][0]
+    R12 = rotation_matrix[0][1]
+    R13 = rotation_matrix[0][2]
+    R21 = rotation_matrix[1][0]
+    R22 = rotation_matrix[1][1]
+    R23 = rotation_matrix[1][2]
+    R31 = rotation_matrix[2][0]
+    R32 = rotation_matrix[2][1]
+    R33 = rotation_matrix[2][2]
+
+    roll = atan2(R32, R33)
+    pitch = atan2(-R31, sqrt(R32*R32+ R33*R33))
+    yaw = atan2(R21, R11)
+
+    # convert from radians to degrees
+    roll = roll * 180/pi 
+    pitch = pitch * 180/pi
+    yaw = yaw * 180/pi 
+    rxyz_deg = [roll , pitch , yaw]
+
+    return rxyz_deg
+
 
 if __name__ == '__main__':
     cap = cv.VideoCapture(0)
