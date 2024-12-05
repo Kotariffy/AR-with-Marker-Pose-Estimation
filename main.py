b1a5968eaaad89895830faa66f84892b4089a581
Kotariffy <iakobzeva@sberbank.ru>
2024-12-05 21:57:47 +0300

    Added velocity parameters

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import numpy as np
 import cv2 as cv
 from cv2 import aruco
-import urllib.request
 from math import (
     asin, pi, atan2, sqrt, cos
 )
+import urllib.request
 import websocket
+from scipy.spatial.transform import Rotation
 
+ARUCO_DICT = {
+    "DICT_4x4_50": cv.aruco.DICT_4X4_50,
+    "DICT_4x4_100": cv.aruco.DICT_4X4_100,
+    "DICT_4x4_250": cv.aruco.DICT_4X4_250,
+    "DICT_4x4_1000": cv.aruco.DICT_4X4_1000,
+    "DICT_5x5_50": cv.aruco.DICT_5X5_50,
+    "DICT_5x5_100": cv.aruco.DICT_5X5_100,
+    "DICT_5x5_250": cv.aruco.DICT_5X5_250,
+    "DICT_5x5_1000": cv.aruco.DICT_5X5_1000,
+    "DICT_6x6_50": cv.aruco.DICT_6X6_50,
+    "DICT_6x6_100": cv.aruco.DICT_6X6_100,
+    "DICT_6x6_250": cv.aruco.DICT_6X6_250,
+    "DICT_6x6_1000": cv.aruco.DICT_6X6_1000
+}
 
 BLUE = (255, 50, 50)
 GREEN = (50, 255, 50)
@@ -28,10 +43,10 @@ def marker_display(corners, ids, rejected, image):
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-            cv.line(image, topLeft, topRight, GREEN, 2)
-            cv.line(image, topRight, bottomRight, GREEN, 2)
-            cv.line(image, bottomRight, bottomLeft, GREEN, 2)
-            cv.line(image, bottomLeft, topLeft, GREEN, 2)
+            cv.line(image, topLeft, topRight, BLUE, 2)
+            cv.line(image, topRight, bottomRight, BLUE, 2)
+            cv.line(image, bottomRight, bottomLeft, BLUE, 2)
+            cv.line(image, bottomLeft, topLeft, BLUE, 2)
 
             # compute and draw the center (x, y) - coordinates of the ArUco marker
             cX = int((topLeft[0] + bottomRight[0]) / 2.0)
@@ -43,80 +58,9 @@ def marker_display(corners, ids, rejected, image):
                        cv.FONT_HERSHEY_PLAIN,
                        0.5,
                        (0, 255, 0),
-                       2)
+                       1)
     return image
 
-# =====================================================================
-
-
-def main():
-
-    calibration_data_path = "calib_data/MultiMatrix.npz"
-    calibration_data = np.load(calibration_data_path)
-    cam_matrix = calibration_data["camMatrix"]
-    dist_coef = calibration_data["distCoef"]
-
-    dictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
-    parameters = aruco.DetectorParameters()
-    detector = cv.aruco.ArucoDetector(dictionary, parameters)
-
-    MARKER_SIZE = 4  # centimeters
-
-    while True:
-        img_resp = urllib.request.urlopen(url)
-        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
-
-        im = cv.imdecode(imgnp, -1)
-
-        # Detecting markers.
-        corners, ids, reject = detector.detectMarkers(im)
-        
-        # Display markers.
-        im = marker_display(corners, ids, reject, im)
-
-        if corners:
-            # Getting rotation and traslation vectors.
-            # rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, MARKER_SIZE, camMatrix, distCoef)
-            rvec, tvec, _ = my_estimatePoseSingleMarkers(corners, MARKER_SIZE, cam_matrix, dist_coef)
-            total_markers = range(0, ids.size)
-            for ids, corners, i in zip(ids, corners, total_markers):
-                # Drawing axisies for the pose of the marker
-                _ = cv.drawFrameAxes(im, cam_matrix, dist_coef, rvec[i], tvec[i], 4, 4)
-
-                corners = corners.reshape(4, 2)
-                corners = corners.astype(int)
-                top_right = corners[0].ravel()
-                bottom_right = corners[2].ravel()
-
-                cv.putText(im, f"id: {ids[0]} Dist: {tvec[i][0]}", top_right, cv.FONT_HERSHEY_PLAIN, 1.3,
-                           PURPLE, 2)
-                cv.putText(im, f"x: {tvec[i][1]} y: {tvec[i][2]}", bottom_right,
-                           cv.FONT_HERSHEY_PLAIN, 1.3, PURPLE, 2)
-                
-
-                x = tvec[i][2][0]
-                y = tvec[i][1][0]
-                z = tvec[i][0][0]
-
-                rotation_matrix, _ = cv.Rodrigues(rvec[i])
-                angle_vec = calculateEulerAngle(rotation_matrix)
-                print_coordinates(i, x, y, z, angle_vec[0], angle_vec[1], angle_vec[2])
-
-                my_msg = str(x) + "," + str(y) + "," + str(z) + "," + str(angle_vec[0]) + "," + str(angle_vec[1]) + "," + str(angle_vec[2]) + '\n'
-    
-                ws.send(my_msg)
-                # Wait for server to respond and print it
-                result = ws.recv()
-                print("Received: " + result)
-
-                # -------------------------------
-
-        cv.imshow('live cam testing', im)
-        key = cv.waitKey(5)
-        if key==ord('q'):
-            break
-
-# ======================================================================
 
 # КРЕН roll -> rotation about x-axis (RED axis)
 # ТАНГАЖ pitch -> rotation about y-axis (GREEN axis)
@@ -134,48 +78,125 @@ def print_coordinates(id, x, y, z, roll, pitch, yaw):
     print('      - pitch:   ', pitch)
     print('      - yaw:     ', yaw)
 
-# ======================================================================
 
-def calculateEulerAngle(rotation_matrix):
-    r11 = rotation_matrix[0][0]
-    r12 = rotation_matrix[0][1]
-    r13 = rotation_matrix[0][2]
-    r21 = rotation_matrix[1][0]
-    r22 = rotation_matrix[1][1]
-    r23 = rotation_matrix[1][2]
-    r31 = rotation_matrix[2][0]
-    r32 = rotation_matrix[2][1]
-    r33 = rotation_matrix[2][2]
+def main():
+    calibration_data_path = "calib_data/MultiMatrix.npz"
+    calibration_data = np.load(calibration_data_path)
+    cam_matrix = calibration_data["camMatrix"]
+    dist_coef = calibration_data["distCoef"]
 
-    if r31 != 1 and r31 != -1:
-        pitch_1 = -1 * asin(r31)
-        pitch_2 = pi - pitch_1
-        roll_1 = atan2(r32 / cos(pitch_1), r33 / cos(pitch_1))
-        roll_2 = atan2(r32 / cos(pitch_2), r33 / cos(pitch_2))
-        yaw_1 = atan2(r21 / cos(pitch_1), r11 / cos(pitch_1))
-        yaw_2 = atan2(r21 / cos(pitch_2), r11 / cos(pitch_2))
+    dictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+    parameters = aruco.DetectorParameters()
+    detector = cv.aruco.ArucoDetector(dictionary, parameters)
 
-        pitch = pitch_1
-        roll = roll_1
-        yaw = yaw_1
-    else:
-        yaw = 0  # anything (we default this to zero)
-        if r31 == -1:
-            pitch = pi / 2
-            roll = yaw + atan2(r12, r13)
+    MARKER_SIZE = 4  # centimeters
+
+    t = 0
+    t, x, y, z, x_angle_deg, y_angle_deg, z_angle_deg, wx, wy, wz, w_angle_x, w_angle_y, w_angle_z = initialize()
+    
+
+    while True:
+
+        img_resp = urllib.request.urlopen(url)
+        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+
+        img = cv.imdecode(imgnp, -1)
+    
+        # Detecting markers.
+        corners, ids, reject = detector.detectMarkers(img)
+
+        # Display markers.
+        # img = marker_display(corners, ids, reject, img)
+
+        objPoints = np.array([[-MARKER_SIZE / 2, MARKER_SIZE / 2, 0],
+                              [MARKER_SIZE / 2, MARKER_SIZE / 2, 0],
+                              [MARKER_SIZE / 2, -MARKER_SIZE / 2, 0],
+                              [-MARKER_SIZE / 2, -MARKER_SIZE / 2, 0]], dtype=np.float32)
+        
+        if ids is not None:
+            for i in range(len(corners)):
+                t = t + 1 
+                if t == 1:
+                    x_angle_deg_temp = 0
+                    y_angle_deg_temp = 0
+                    z_angle_deg_temp = 0
+                    x_temp = 0
+                    y_temp = 0
+                    z_temp = 0
+                
+                else:
+                    x_angle_deg_temp = x_angle_deg
+                    y_angle_deg_temp = y_angle_deg
+                    z_angle_deg_temp = z_angle_deg
+                    x_temp = x
+                    y_temp = y
+                    z_temp = z
+
+                
+                success, rvec, tvec = cv.solvePnP(objPoints, corners[i], cam_matrix, dist_coef, flags=cv.SOLVEPNP_ITERATIVE)
+
+                if success:
+                    print(f'Шаг: {t}')
+                    cv.drawFrameAxes(img, cam_matrix, dist_coef, rvec, tvec, MARKER_SIZE * 1.5, 2)
+                    print(f'Расстояние:  {tvec.flatten()} мм')
+                    x = tvec[0][0]
+                    y = tvec[1][0]
+                    z = tvec[2][0]
+
+                    rotation_matrix, _ = cv.Rodrigues(rvec)
+                    r = Rotation.from_matrix(rotation_matrix)
+                    euler_angles = r.as_euler('ZYX', degrees=True)
+                    z_angle_deg, y_angle_deg, x_angle_deg = euler_angles
+
+                    if t > 1:
+                        w_angle_x = x_angle_deg - x_angle_deg_temp
+                        w_angle_y = y_angle_deg - y_angle_deg_temp
+                        w_angle_z = z_angle_deg - z_angle_deg_temp 
+                        wx = x - x_temp
+                        wy = y - y_temp
+                        wz = z - z_temp
+
+                    print(f"Скорости поворота: wx={wx}, wy={wy}, wz={wz}", end='\n')
+                    print(f"Скорости поворота углов: {w_angle_x}, {w_angle_y}, {w_angle_z}", end='\n')
+                    print(f"Углы повороты: x={x_angle_deg}, y={y_angle_deg}, z={z_angle_deg}", end='\n\n')
+                    
+                    # Send to esp
+                    my_msg = str(x) + "," + str(y) + "," + str(z) + "," + str(wx) + "," + str(wy) + "," + str(wz) + "," + str(x_angle_deg) + "," + str(y_angle_deg) + "," + str(z_angle_deg) + "," + str(w_angle_x) + "," + str(w_angle_y) + "," + str(w_angle_z) + '\n'
+                    ws.send(my_msg)
+                    # Wait for server to respond and print it
+                    result = ws.recv()
+                    print("Received: " + result)
+                    
+
+            aruco.drawDetectedMarkers(img, corners, ids, GREEN)
+            
         else:
-            pitch = -pi / 2
-            roll = -1 * yaw + atan2(-1 * r12, -1 * r13)
+            t = 0
+        # -------------------------------
 
-            # convert from radians to degrees
-    roll = roll * 180 / pi
-    pitch = pitch * 180 / pi
-    yaw = yaw * 180 / pi
-    rxyz_deg = [roll, pitch, yaw]
+        # Display the resulting frame with axies.
+        cv.imshow('live cam testing', img)
+        key = cv.waitKey(5)
+        if key==ord('q'):
+            break
 
-    return rxyz_deg
+    
+def initialize():
+    t = 0
+    x = 0
+    y = 0
+    z = 0
+    y_angle_deg = 0
+    x_angle_deg = 0
+    z_angle_deg = 0
+    wx = 0
+    wy = 0
+    wz = 0
+    w_angle_x = 0
+    w_angle_y = 0
+    w_angle_z = 0
 
-# ======================================================================
+    return t, x, y, z, x_angle_deg, y_angle_deg, z_angle_deg, wx, wy, wz, w_angle_x, w_angle_y, w_angle_z
 
 def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
     marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
@@ -191,8 +212,6 @@ def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
         tvecs.append(t)
         trash.append(success)
     return rvecs, tvecs, trash
-
-# ======================================================================
 
 if __name__ == '__main__':
 
